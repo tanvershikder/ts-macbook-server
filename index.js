0e982357ec3fd3e542885b33e225c7a1fa272a12
Your Name <you@example.com>
2022-05-05 10:20:46 +0600

    delte methode done

--- index.js
+++ index.js
@@ -73,7 +73,12 @@ async function run(){
         })
 
         //delete 
-        
+        app.delete('/products/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await mackbookCollection.deleteOne(query);
+            res.send(result)
+        })
     }
     finally{
 
