d3fb2d2e6b2f5f1dee78586027fe86eb650c82ab
Your Name <you@example.com>
2022-05-05 10:19:47 +0600

    quantity update

--- index.js
+++ index.js
@@ -73,12 +73,7 @@ async function run(){
         })
 
         //delete 
-        app.delete('/products/:id',async(req,res)=>{
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)}
-            const result = await mackbookCollection.deleteOne(query);
-            res.send(result)
-        })
+        
     }
     finally{
 
