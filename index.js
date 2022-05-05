7956ec67c9e1a8e914e27a1bdc85df749f8a81d0
Your Name <you@example.com>
2022-05-05 15:35:02 +0600

    add item verify with jwt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 4000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { verify } = require('jsonwebtoken');
 
 
 require('dotenv').config();
@@ -18,29 +19,29 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ttauu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect()
         const mackbookCollection = client.db('ts-mackbook').collection('mackbooks')
 
         //get all post item and shaow all
-        app.get('/products', async(req,res)=>{
+        app.get('/products', async (req, res) => {
             const cursor = mackbookCollection.find({});
             const result = await cursor.toArray()
             res.send(result)
         })
-    
+
         // get specific item 
-        app.get('/products/:id', async(req,res)=>{
+        app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = await mackbookCollection.findOne(query);
             res.send(result)
             console.log("loaded");
         })
 
         //get my item
-        app.get('/product', async(req,res)=>{
+        app.get('/product', async (req, res) => {
             const query = req.query;
             console.log(query);
             const cursor = mackbookCollection.find(query)
@@ -49,34 +50,51 @@ async function run(){
         })
 
         //post products
-        app.post('/products',async(req,res)=>{
+        app.post('/products', async (req, res) => {
             const newProduct = req.body;
-            const result = await mackbookCollection.insertOne(newProduct)
-            res.send(result)
+            const tokenInfo = req.header.authorization
+
+            const [email, accesstoken] = tokenInfo.split(" ")
+            console.log(accesstoken);
+
+            const decoded = verfyToken(accesstoken)
+            console.log(decoded.email);
+
+            if (email !== decoded.email) {
+                res.send({ success: "unAuthorized user" })
+
+            }
+
+            else {
+                const result = await mackbookCollection.insertOne(newProduct)
+                res.send(result)
+                res.send({success:"service add successfully"})
+
+            }
         })
 
         // Update quantity
-        app.put('/products/:id',async(req,res)=>{
+        app.put('/products/:id', async (req, res) => {
             const id = req.params.id;
             console.log(id);
             const quantity = req.body.updatequantity;
             console.log(quantity);
             console.log(quantity);
-            const filter = {_id: ObjectId(id)}
-            const option = {upsert: true };
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true };
             const updatePro = {
                 $set: {
                     quantity
                 }
             }
-            const result = await mackbookCollection.updateOne(filter,updatePro,option);
+            const result = await mackbookCollection.updateOne(filter, updatePro, option);
             res.send(result)
         })
 
         //delete 
-        app.delete('/products/:id',async(req,res)=>{
+        app.delete('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = await mackbookCollection.deleteOne(query);
             res.send(result)
         })
@@ -87,13 +105,13 @@ async function run(){
             // console.log(email);
 
             const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
-            console.log("token ",token);
+            console.log("token ", token);
             res.send({ token: token })
-            
+
         })
 
     }
-    finally{
+    finally {
 
     }
 
@@ -105,8 +123,8 @@ run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('ts-mackbook is running')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`port is running on`, port)
-  })
+})
